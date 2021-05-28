725ba81d35f98f3f93971bfd824ade38bbe86327
Xlioxide <80074222+Xlioxide@users.noreply.github.com>
2021-05-28 10:37:14 -0400

    Update Main.py

--- MistyNuker/Main.py
+++ MistyNuker/Main.py
@@ -189,7 +189,7 @@ async def zoom(self, ctx):
         ##Bans all members in the guild the command is used##
         await ctx.message.delete()
         print(
-        f"{Fore.RED}[-]banAll > {Fore.RESET}Starting to ban all members of {ctx.guild}"
+        f"{Fore.RED}[-]banAll > {Fore.RESET}Starting to ban all members"
         )
         for member in ctx.guild.members:
             try:
